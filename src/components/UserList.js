15d06bd94415d8d86725f563a652b8454a687f67
Brian <b.perez3237@gmail.com>
2022-05-05 18:37:38 -0400

    Finish UserList Component

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
 function UserList({feed}) {
+    const [search, setSearch] = useState('')
 
+    
+    function handleChange(e) {
+        setSearch(e.target.value)
+    }
 
-
+    const users = []
+    feed.forEach((post)=>users.push(post.user))
+    const uniqueUsers = [...new Set(users)]
+    const userListElements = uniqueUsers.map((user) => {
+        if (user.includes(search)) {
+            return <li>{user}</li>
+        }
+    })
 
     return (
-        <h1>User List:</h1>
+        <div>
+            <h1>User List:</h1>
+            <h2>Filter List:</h2>
+            <input type='text' value={search} onChange={handleChange} />
+            <ul>
+                {userListElements}
+            </ul>
+        </div>
     )
 }
 
