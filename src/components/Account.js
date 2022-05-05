d1c04187da97278be9b9c65caab0d34d96509d54
Brian <b.perez3237@gmail.com>
2022-05-05 16:57:28 -0400

    Updated account component

--- src/components/Account.js
+++ src/components/Account.js
@@ -3,12 +3,22 @@ import React from "react";
 function Account({username, feed}) {
 
     const userFeed = feed.filter((post)=>post.user===username)
+   
+    let counter = 0
+    userFeed.forEach((post)=>{
+        console.log(post.text.length)
+        counter = counter+ post.text.length
+    })
+
+    
+
 
     return (
         <div>
             <h1>Account Owner:</h1>
             <h2>{username}</h2>
             <h2>Number of Posts: {userFeed.length}</h2>
+            <h2>Characters Typed: {counter}</h2>
         </div>
     )
 }
